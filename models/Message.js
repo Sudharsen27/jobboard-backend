c89f0163be40fdce361f3497ca484bda0c6c764a
Sudharsen27 <sundarlingam272000@gmail.com>
2025-07-14 18:21:40 +0530

    Executed message section

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,22 @@
+// const mongoose = require('mongoose');
+
+// const messageSchema = new mongoose.Schema({
+//   jobId: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
+//   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   message: String,
+//   timestamp: { type: Date, default: Date.now },
+// });
+
+// module.exports = mongoose.model('Message', messageSchema);
+
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  jobId: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  message: String,
+  jobId: { type: String, required: true },         // Changed from ObjectId to String
+  senderId: { type: String, required: true },      // Changed from ObjectId to String
+  receiverId: { type: String, required: true },    // Changed from ObjectId to String
+  message: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
